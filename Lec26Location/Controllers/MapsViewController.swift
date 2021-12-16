e1a3798380a33eff07dfe1d823988bc67d6cc31e
Tomer <tomer.bu@gmail.com>
2021-12-16 22:03:48 +0200

    initial commit

---
+++ Lec26Location/Controllers/MapsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapsViewController.swift
+//  Lec26Location
+//
+//  Created by Tomer Buzaglo
+//
+//
+
+import UIKit
+import MapKit
+
+class MapsViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func mapTypeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .satellite
+        case 2:
+            mapView.mapType = .hybrid
+        default:
+            fatalError("No such segment")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Lec26Location/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Lec26Location
+//
+//  Created by Tomer Buzaglo 
+//  
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
